18039820832d4c29debf6fb81221ce11ccf109d2
Matthew Bloch <mjbloch22@gmail.com>
2024-02-11 14:27:50 -0800

    working version of API

--- api/config.py
+++ api/config.py
@@ -1,29 +1,29 @@
 import os
-# from google.cloud.sql.connector import Connector, IPTypes
-# import sqlalchemy
-# import sys
+from google.cloud.sql.connector import Connector, IPTypes
+import sqlalchemy
+import sys
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Python Connector database connection function
-# def getconn():
-#     db_user = os.environ["DB_USER"]
-#     db_pass = os.environ["DB_PASS"]
-#     db_name = os.environ["DB_NAME"]
-#     instance_connection_name = os.environ["INSTANCE_CONNECTION_NAME"]
+def getconn():
+    db_user = os.environ["DB_USER"]
+    db_pass = os.environ["DB_PASS"]
+    db_name = os.environ["DB_NAME"]
+    instance_connection_name = os.environ["INSTANCE_CONNECTION_NAME"]
     
-#     with Connector() as connector:
-#         conn = connector.connect(
-#             instance_connection_name, # Cloud SQL Instance Connection Name
-#             "pg8000",
-#             user=db_user,
-#             password=db_pass,
-#             db=db_name,
-#             ip_type= IPTypes.PUBLIC  # IPTypes.PRIVATE for private IP
-#         )
-#         return conn
+    with Connector() as connector:
+        conn = connector.connect(
+            instance_connection_name, # Cloud SQL Instance Connection Name
+            "pg8000",
+            user=db_user,
+            password=db_pass,
+            db=db_name,
+            ip_type= IPTypes.PUBLIC  # IPTypes.PRIVATE for private IP
+        )
+        return conn
 
-# Test
+# # Test
 # def db_connection_string():
 #     """Returns db connection URI"""
 
@@ -47,8 +47,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 #         print(db_url, file=sys.stderr)
 #         return db_url
 
-# class Config:
-    # SQLALCHEMY_DATABASE_URI = "postgresql+pg8000://"
-    # SQLALCHEMY_ENGINE_OPTIONS = {"creator": getconn}
+class Config:
     # SQLALCHEMY_DATABASE_URI = db_connection_string()
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = "postgresql+pg8000://"
+    SQLALCHEMY_ENGINE_OPTIONS = {"creator": getconn}
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
