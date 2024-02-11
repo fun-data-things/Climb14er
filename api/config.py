5df73fc948e1e39d9b7df2843243ad41f064f6ca
Matthew Bloch <mjbloch22@gmail.com>
2024-02-11 13:09:03 -0800

    test dev env ci/ci

--- api/config.py
+++ api/config.py
@@ -25,24 +25,27 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Test
 def db_connection_string():
     """Returns db connection URI"""
-    db_user = os.environ["DB_USER"]
-    db_pass = os.environ["DB_PASS"]
-    db_name = os.environ["DB_NAME"]
-    db_port = os.environ["DB_PORT"]
-    unix_socket_path = os.environ["INSTANCE_UNIX_SOCKET"]
 
-    db_url = sqlalchemy.engine.url.URL.create(
-        drivername="postgresql+pg8000",
-        username=db_user,
-        password=db_pass,
-        database=db_name,
-        query={"unix_sock": f"{unix_socket_path}/.s.PGSQL.{db_port}"},
-    )
+    if "DEV_DB_URI" in os.environ:
+        return os.environ["DEV_DB_URI"]
 
-    return db_url
+    else:
+        db_user = os.environ["DB_USER"]
+        db_pass = os.environ["DB_PASS"]
+        db_name = os.environ["DB_NAME"]
+        db_port = os.environ["DB_PORT"]
+        unix_socket_path = os.environ["INSTANCE_UNIX_SOCKET"]
+
+        db_url = sqlalchemy.engine.url.URL.create(
+                drivername="postgresql+pg8000",
+                username=db_user,
+                password=db_pass,
+                database=db_name,
+                query={"unix_sock": f"{unix_socket_path}/.s.PGSQL.{db_port}"},
+            )
+        return db_url
 
 class Config:
-#     # SECRET_KEY = os.environ.get('SECRET_KEY')
     # SQLALCHEMY_DATABASE_URI = "postgresql+pg8000://"
     # SQLALCHEMY_ENGINE_OPTIONS = {"creator": getconn}
     SQLALCHEMY_DATABASE_URI = db_connection_string()
--- api/main.py
+++ api/main.py
@@ -2,4 +2,4 @@ from app import app
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080, static_folder='../build', static_url_path='/')
+    app.run(debug=True, port=8080, static_folder='../static', static_url_path='/')
