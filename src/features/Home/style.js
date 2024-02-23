1fe85760b68fc2e7959281c3ebb2d278e51a65d5
Matthew Bloch <mjbloch22@gmail.com>
2024-02-22 18:52:04 -0800

    Feature: home page

---
+++ src/features/Home/style.js
@@ -0,0 +1,41 @@
+const HomePageContainer = {
+    paddingBottom: '40px'
+};
+
+const HomeImageContainer = {
+    borderRadius: '2px',
+};
+
+const HomeImage = {
+    maxWidth: '800px'
+};
+
+const HomeHeaderContainer = {
+    fontSize: '18px',
+    fontWeight: '500',
+    lineHeight: '28px',
+    padding: '18px 24px',
+    display: 'inline-block',
+    maxWidth: '700px'
+};
+
+const AboutContainer = {
+    padding: '12px 24px',
+    lineHeight: '28px',
+    fontSize: '16px',
+    display: 'inline-block',
+    maxWidth: '700px'
+}
+
+const AboutContainerDetail = {
+    padding: '12px'
+}
+
+export {
+    HomePageContainer,
+    HomeImageContainer,
+    HomeImage,
+    HomeHeaderContainer,
+    AboutContainer,
+    AboutContainerDetail
+};
