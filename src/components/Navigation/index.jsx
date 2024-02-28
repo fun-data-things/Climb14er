f7dec0eedeb5e5d3e1653c84f1d3b1813ebc67e7
Matthew Bloch <mjbloch22@gmail.com>
2024-02-28 15:59:12 -0800

    Mobile UI updates

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,14 +1,34 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { NavContainer, NavLink } from './style';
+import HamburgerNav from './HamburgerNav';
+import * as S from './style';
 
 const Navigation = () => {
+    const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+    const toggleHamburger = () => {
+        setHamburgerOpen((prevState) => !prevState);
+    };
+
     return (
-        <div style={NavContainer}>
-            <Link to="/" style={NavLink}>Climb14er</Link>
-            <Link to="/plan" style={NavLink}>Plan</Link>
-            <Link to="/explore" style={NavLink}>Explore</Link>
-            <Link to="https://evidence-ov7neagfqq-uw.a.run.app" target="_blank" style={NavLink}>Analytics</Link>
-        </div>
+        <>
+            <div style={S.FormatNavContainer(hamburgerOpen)}>
+                <div className='navigation'>
+                    <Link to="/" style={S.NavLink}>Climb14er</Link>
+                    <Link to="/plan" style={S.NavLink}>Plan</Link>
+                    <Link to="/explore" style={S.NavLink}>Explore</Link>
+                    <Link to="https://evidence-ov7neagfqq-uw.a.run.app" target="_blank" style={S.NavLink}>Analytics</Link>
+                </div>
+                <HamburgerNav hamburgerOpen={hamburgerOpen} onClick={toggleHamburger}> 
+                    <div style={S.HamburgerLinkContainer}>
+                        <Link to="/" style={S.HamburgerNavLink} onClick={toggleHamburger}>Climb14er</Link>
+                        <Link to="/plan" style={S.HamburgerNavLink} onClick={toggleHamburger}>Plan</Link>
+                        <Link to="/explore" style={S.HamburgerNavLink} onClick={toggleHamburger}>Explore</Link>
+                        <Link to="https://evidence-ov7neagfqq-uw.a.run.app" target="_blank" style={S.HamburgerNavLink} onClick={toggleHamburger}>Analytics</Link>
+                    </div>
+                </HamburgerNav>
+            </div>
+        </>
     )
 };
 
--- src/components/Navigation/style.js
+++ src/components/Navigation/style.js
@@ -1,10 +1,12 @@
-const NavContainer = {
-    display: 'flex', 
-    justifyContent: 'start', 
-    backgroundColor: '#104b0e', 
-    color: 'white',
-    height: '45px',
-    width: 'auto'
+const FormatNavContainer = (hamburgerOpen) => {
+    return {
+        display: 'flex', 
+        justifyContent: 'start', 
+        backgroundColor: '#104b0e', 
+        color: 'white',
+        height: '45px',
+        width: hamburgerOpen ? '0' : 'auto'
+    }
 };
 
 const NavLink = {
@@ -15,4 +17,49 @@ const NavLink = {
     padding: '0 22px'
 };
 
-export { NavContainer, NavLink };
+const HamburgerNavLink = {
+    ...NavLink,
+    display: 'inline-block',
+    margin: '20px',
+    width: '50%'
+};
+
+const HamburgerContainter = {
+    width: '2rem',
+    height: '2rem',
+    display: 'flex',
+    justifyContent: 'space-around',
+    flexFlow: 'wrap',
+    zIndex: '10',
+    cursor: 'pointer',
+    position: 'fixed'
+};
+
+const Burger = {
+    width: '2rem',
+    height: '3px',
+    borderRadius: '6px',
+    backgroundColor: 'white',
+    transformOrigin: '1px',
+    transition: 'all 0.3s linear'
+};
+
+const HamburgerLinkContainer = {
+    backgroundColor: '#104b0e',
+    width: '50%',
+    left: '0',
+    position: 'fixed',
+    marginTop: '-22px',
+    height: '100%',
+    paddingTop: '60px',
+    zIndex: '9'
+}
+
+export {
+    NavLink,
+    HamburgerContainter,
+    Burger,
+    HamburgerLinkContainer,
+    HamburgerNavLink,
+    FormatNavContainer
+};
